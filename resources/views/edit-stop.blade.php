d49fe0dc52536fa8b854d5b937ac8dd9728fbb98
Umeshww <kashishverm1910@gmail.com>
2026-06-26 10:00:08 +0530

    change

--- resources/views/edit-stop.blade.php
+++ resources/views/edit-stop.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Edit Stop with Map</title>
     <!-- Tailwind CSS CDN -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- Leaflet Map CSS -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />

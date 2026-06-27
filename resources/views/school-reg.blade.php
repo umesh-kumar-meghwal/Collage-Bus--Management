0cfaaf144a30f357ebe732eddc5df99b6806c1d6
Umeshww <kashishverm1910@gmail.com>
2026-06-27 14:38:47 +0530

    change

--- resources/views/school-reg.blade.php
+++ resources/views/school-reg.blade.php
@@ -1,5 +1,5 @@
 <h1>School Register</h1>
-<p>School Name : <input type="text" name="school_name" id="school_id" placeholder="Enter The School Name" required></p>
+<p>School Name : <input type="text" name="school_name" id="school_name" placeholder="Enter The School Name" required></p>
 <p>School Email : <input type="text" name="school_email" id="school_email" placeholder="Enter The School Email " required></p>
 <p>Phone : <input type="text" name="phone" id="phone" placeholder="Enter The Phone " required>
 <p>Address : <input type="text" name="address" id="address" placeholder="Enter the address" required></p>
